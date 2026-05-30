103da27f533354e8f5768db10588e49db3620bc9
ShalinShikhar <Shalin@5207>
2026-05-30 21:13:59 +0530

    security started

--- main/java/com/example/lovable_clone/mapper/UserMapper.java
+++ main/java/com/example/lovable_clone/mapper/UserMapper.java
@@ -1,5 +1,6 @@
 package com.example.lovable_clone.mapper;
 
+import com.example.lovable_clone.dto.auth.SignUpRequest;
 import com.example.lovable_clone.dto.auth.UserProfileResponse;
 import com.example.lovable_clone.dto.members.MemberResponse;
 import com.example.lovable_clone.entity.User;
@@ -9,4 +10,6 @@ import org.mapstruct.Mapper;
 public interface UserMapper {
     UserProfileResponse toUserProfileResponse(User user);
     MemberResponse toMemberResponse(User user);
+    User toEntity(SignUpRequest request);
+
 }
--- main/java/com/example/lovable_clone/service/Impl/AuthServiceimpl.java
+++ main/java/com/example/lovable_clone/service/Impl/AuthServiceimpl.java
@@ -3,15 +3,36 @@ package com.example.lovable_clone.service.Impl;
 import com.example.lovable_clone.dto.auth.AuthResponse;
 import com.example.lovable_clone.dto.auth.LoginRequest;
 import com.example.lovable_clone.dto.auth.SignUpRequest;
+import com.example.lovable_clone.entity.User;
+import com.example.lovable_clone.error.BadRequestException;
+import com.example.lovable_clone.mapper.UserMapper;
+import com.example.lovable_clone.repository.UserRepository;
 import com.example.lovable_clone.service.AuthService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.jspecify.annotations.Nullable;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true,level = AccessLevel.PRIVATE)
 public class AuthServiceimpl implements AuthService {
+
+    UserRepository userRepository;
+    UserMapper userMapper;
+    PasswordEncoder passwordEncoder;
     @Override
     public AuthResponse signup(SignUpRequest request) {
-        return null;
+        userRepository.findByUsername(request.username()).ifPresent(user->{
+            throw new BadRequestException("User already exist with username"+request.username());
+
+        });
+        User user=userMapper.toEntity(request);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return new AuthResponse("dummy",userMapper.toUserProfileResponse(user));
     }
 
     @Override
